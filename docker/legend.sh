b891fb9c562ef5c6ce60c77973b441f8b0194bba
Antoine Amend <antoine.amend@gmail.com>
2022-08-19 09:35:31 -0700

    Adding mongodb

--- docker/legend.sh
+++ docker/legend.sh
@@ -7,6 +7,7 @@ configure)
   ${LEGEND_HOME}/setup.sh
   ;;
 start)
+  $0 configure
   sudo docker-compose --env-file ${LEGEND_HOME}/dist/environment up --detach
   ;;
 stop)
@@ -16,8 +17,18 @@ restart)
   $0 stop
   $0 start
   ;;
+clean)
+  $0 stop
+  rm -rf ${LEGEND_HOME}/dist
+  IMAGES=`sudo docker images | awk '{print $3}' | grep -v IMAGE`
+  for IMAGE in $IMAGES:
+  do
+    sudo docker rmi $IMAGE
+  done
+  sudo docker volume prune
+  ;;
 *)
-   echo "Usage: $0 {start|stop|restart|configure}"
+   echo "Usage: $0 {start|stop|restart|configure|clean}"
 esac
 
 exit 0
